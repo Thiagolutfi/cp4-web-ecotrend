3f230371ba4841ecd09161b25202db1319e0bd67
Thiago <lutfithiago@gmail.com>
2026-08-31 16:23:32 -0300

    gitignore e remove o node_modules

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,10 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/cp4-web-ecotrend/',
   plugins: [
     react(),
     tailwindcss(),
   ],
 })
+
